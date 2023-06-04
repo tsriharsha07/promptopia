9aa72eadd99cfa6a575f7d1f991b01f09c03f89d
tsriharsha07 <e19cse031@bennett.edu.in>
2023-06-04 16:10:24 +0530

    Completed Project

---
+++ models/prompt.js
@@ -0,0 +1,20 @@
+import mongoose, {Schema,model,models} from 'mongoose'
+
+const promptSchema=new Schema({
+    creator:{
+        type:Schema.Types.ObjectId,
+        ref:'User',     
+    },
+    prompt:{
+        type:String,
+        required:[true,'Prompt is Required']
+    },
+    tag:{
+        type:String,
+        required:[true,'Tag is Required']
+    }
+})
+
+const Prompt=models.Prompt || model("Prompt",promptSchema);
+
+export default Prompt;
